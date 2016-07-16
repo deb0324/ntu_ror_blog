ff296b3fe77e74b0f6a71b50aa538d56c186ad59
Debbie Tseng <debbie_wei0324@hotmail.com>
2016-07-16 15:10:30 +0800

    added validation to user.rb

--- myBlog/app/models/user.rb
+++ myBlog/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :name, length: { maximum: 10 }
+
+  validates :name, uniqueness: true
+  #validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters is allowed" }
 end
