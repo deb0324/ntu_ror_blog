30bb2afab61f2c6dba1d4952928bd08b887d4293
Debbie Tseng <debbie_wei0324@hotmail.com>
2016-07-17 15:23:39 +0800

    added index and show page

--- myBlog/config/routes.rb
+++ myBlog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'users/' => 'users#index'
+  resources :posts
+
+  #get '/index' => 'posts#index'
+
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
