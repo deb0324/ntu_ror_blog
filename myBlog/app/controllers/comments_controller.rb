9162cacb3162f095988e6cbc6f82f1bfc68b27af
Debbie Tseng <debbie_wei0324@hotmail.com>
2016-07-21 16:42:20 +0800

    added nickname to comments

--- myBlog/app/controllers/comments_controller.rb
+++ myBlog/app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.create(comment_params)
     @post.comments << @comment
+    @comment.user = current_user
 
 
     if @comment.save!
